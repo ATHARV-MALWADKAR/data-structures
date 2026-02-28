7277c45a34763e61393004bc8aa35cd21aac719a
Atharv <atharvmalwadkar17@gmail.com>
2026-02-28 17:40:36 +0530

    Add basic array operations in Python

---
+++ arrays.py
@@ -0,0 +1,16 @@
+# Array operations in Python
+
+arr = [10, 20, 30, 40, 50]
+
+print("Array:", arr)
+
+# Access element
+print("First element:", arr[0])
+
+# Add element
+arr.append(60)
+print("After append:", arr)
+
+# Remove element
+arr.remove(30)
+print("After remove:", arr)
